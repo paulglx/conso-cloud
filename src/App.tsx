7191f62a47d955884cc1b99e2c1afba9f85b783c
EneVyctis <roux.basi@gmail.com>
2025-01-16 01:02:55 +0100

    added cloud providers comparison and reference numbers

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,9 @@ function App() {
   const [cloudProvider, setCloudProvider] = useState<CloudProvider>("AWS");
   const [region, setRegion] = useState<string>(REGIONS_BY_PROVIDER.AWS[0].id);
 
+  const [cloudProviderBis, setCloudProviderBis] = useState<CloudProvider>("AWS");
+  const [regionBis, setRegionBis] = useState<string>(REGIONS_BY_PROVIDER.AWS[0].id);
+
   const sources: SourceConfig[] = [
     {
       title: "Stockage",
@@ -69,6 +72,16 @@ function App() {
     1,
   );
 
+  const co2ImpactBis = roundToDecimals(
+    totalImpact * CO2_INTENSITY[cloudProviderBis][regionBis] * 1000,
+    1,
+  );
+
+  const individualReferenceImpact = roundToDecimals(
+    co2Impact / 9200,
+    1,
+  );
+
   const handleSourceChange = (title: string, value: number) => {
     setSourceValues((prev) => ({ ...prev, [title]: value }));
   };
@@ -76,6 +89,12 @@ function App() {
   const handleProviderChange = (newProvider: CloudProvider) => {
     setCloudProvider(newProvider);
     setRegion(REGIONS_BY_PROVIDER[newProvider][0].id);
+  };
+
+
+  const handleProviderChangeBis = (newProvider: CloudProvider) => {
+    setCloudProviderBis(newProvider);
+    setRegionBis(REGIONS_BY_PROVIDER[newProvider][0].id);
   };
 
   return (
@@ -117,6 +136,7 @@ function App() {
             ))}
           </select>
         </div>
+
         {sources.map((config) => (
           <Source
             key={config.title}
@@ -125,6 +145,7 @@ function App() {
             onValueChange={(value) => handleSourceChange(config.title, value)}
           />
         ))}
+
         <div className="w-full bg-green-50 border border-green-200 p-10 rounded-xl col-span-2">
           <h3 className="text-lg text-green-700 font-bold">
             Consommation totale
@@ -142,7 +163,83 @@ function App() {
             &nbsp;émis par an
           </span>
         </div>
+
+        <div className="w-full bg-zinc-50 border border-zinc-200 p-10 rounded-xl col-span-3">
+          <h3 className="text-lg text-zinc-700 font-bold">
+            Comparaison avec un autre service cloud
+          </h3>
+        </div>
+
+        <div className="w-full bg-zinc-50 border p-10 rounded-xl">
+          <h3 className="text-lg text-zinc-800 font-bold">Cloud Provider</h3>
+          <select
+            value={cloudProviderBis}
+            onChange={(e) =>
+              handleProviderChangeBis(e.target.value as CloudProvider)
+            }
+            className="mt-2 w-full p-2 rounded border border-zinc-200"
+          >
+            <option value="AWS">AWS</option>
+            <option value="GCP">GCP</option>
+            <option value="Azure">Azure</option>
+          </select>
+        </div>
+        <div className="w-full bg-zinc-50 border p-10 rounded-xl">
+          <h3 className="text-lg text-zinc-800 font-bold">Region</h3>
+          <select
+            value={regionBis}
+            onChange={(e) => setRegionBis(e.target.value)}
+            className="mt-2 w-full p-2 rounded border border-zinc-200"
+          >
+            {REGIONS_BY_PROVIDER[cloudProviderBis].map((region) => (
+              <option key={region.id} value={region.id}>
+                {region.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="w-full bg-zinc-50 border border-zinc-200 p-10 rounded-xl col-span-1">
+          <h3 className="text-lg text-zinc-700 font-bold">
+            Consommation totale
+          </h3>
+          <span className="text-4xl font-black text-zinc-800 geist-mono">
+            {totalImpact}
+          </span>
+          <span className="text-zinc-700 font-semibold text-lg">
+            &nbsp;kWh / an
+          </span>
+          <br />
+          <span className="text-zinc-600">
+            soit&nbsp;
+            <span className="font-semibold geist-mono">{co2ImpactBis} kgCO2e</span>
+            &nbsp;émis par an
+          </span>
+        </div>
+
+        <div className="w-full bg-zinc-50 border border-zinc-200 p-10 rounded-xl col-span-3">
+          <h3 className="text-lg text-zinc-700 font-bold">
+            Quelques chiffres de référence
+          </h3>
+          <span className="text-zinc-600">
+            <span className="text-4xl font-black text-zinc-800 geist-mono">
+              {co2Impact}
+            </span>
+            <span className="text-zinc-700 font-semibold text-lg">
+              &nbsp;kgCO2e
+            </span>
+            <br />
+            Cela correspond aux émissions carbones moyennes de &nbsp;
+            <span className="font-semibold geist-mono">{individualReferenceImpact} habitants</span>
+            &nbsp; en France chaque année.
+          </span>
+        </div>
+
+
+
       </div>
+
+
 
       <div className="mt-6 px-12 py-10 bg-zinc-50 rounded-xl border">
         <h3 className="text-lg font-bold text-zinc-700 mb-2">Sources</h3>
